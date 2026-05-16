f4753f4fb4d4f13eb559231d2e69f874cedb01aa
ZIad El-Bahy <118315573+elbahy999@users.noreply.github.com>
2026-05-16 20:15:28 +0300

    Add button click event to display customer count

---
+++ Models/piece of cpde/test button.cpp
@@ -0,0 +1,23 @@
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = DatabaseHelper.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+
+                    string query = "SELECT COUNT(*) FROM Customers";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    int count = (int)cmd.ExecuteScalar();
+
+                    MessageBox.Show("Total Customers Registered: " + count);
+                }
+                catch (Exception ex)
+                {
+                    // Member 6 will eventually handle this logging to a .txt file
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
